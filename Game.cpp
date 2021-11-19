27cc16baded6fc934a28f235de80d03f084be151
Seonhyeok Kim <90969155+justinkim1998@users.noreply.github.com>
2021-11-19 22:38:11 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -7,9 +7,9 @@ int main(){
   
   cout << "=======WELCOME TO THE FIGHT GAME.=======" << endl << endl;
   while (True){
-    cout << "Choose a character." << endl << "1.KNIGHT 2.MAGE 3.ASSASSIN 4.ARCHER" << endl << endl;
-    cin >> choose;
-    switch (choose){
+    cout << "Choose a character." << endl << "1.KNIGHT 2.MAGE 3.ASSASSIN 4.SHOOTER" << endl << endl;
+    cin >> character;
+    switch (character){
       case 1:
         cout << "You have chosen the KNIGHT." << endl;
         playerHP=500;
@@ -91,6 +91,7 @@ int main(){
     cout << "Monster HP: " << monsterHP << endl << "Monster MP: " << monsterMP << endl << "Monster Attack: " << monsterATK << endl << "Monster Defense: " << monsterDEF << endl << "Monster Avoid: " << monsterAVD << endl << "Monster Critical: " << monsterCRT << endl << "============================================" << endl;
     
     int playerChoice;
+    int playerSkillChoice;
     
     while (True){
       if (playerHP > 0 && monsterHP > 0){
@@ -101,9 +102,201 @@ int main(){
         cin >> playerChoice;
         switch (playerChoice){
           case 1: 
+            CRT = rand % 101 + playerCRT;
+            if (CRT > 100){
+              playerDMG = playerATK * 1.3 -monsterDEF;
+              monsterHP -= playerDMG;
+              cout << "Critical hit!!! Damage: " << playerDMG << endl;
+            }
+            else {
+              playerDMG = playerATK - monsterDEF;
+              monsterHP -= playerDMG;
+              cout << "You have attacked the monster! Damage: " << playerDMG << endl;
+            }
             break;
             
           case 2:
+            cout << "Which skill will you use?" << endl;
+            if (character == 1){
+              cout << "1. ALPHA STRIKE (Damage: AAA, Mana Required: XXX) / 2. JUDGMENT (Damage: BBB, Mana Required: YYY) / 3. MEDITATE (HP +CCC, Mana Required: ZZZ)" << endl;
+              cin >> playerSkillChoice;
+              switch (playerSkillChoice){
+                case 1:
+                  if (playerMP >= XXX){
+                    playerDMG = AAA - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= XXX;
+                    cout << "Player's ALPHA STRIKE! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 2:
+                  if (playerMP >= YYY){
+                    playerDMG = BBB - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= YYY;
+                    cout << "Player's JUDGMENT! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 3:
+                   if (playerMP >= ZZZ){
+                    playerHP += CCC;
+                    playerMP -= ZZZ;
+                    cout << "Player's MEDITATE! HP + " << CCC << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                default:
+                  cout << "Invalid choice." << endl;
+                  break;
+              }
+            }
+            
+            else if (character == 2){
+              cout << "1. ETHEREAL CHAINS (Damage: AAA, Mana Required: XXX) / 2. DISTORTION (Damage: BBB, Mana Required: YYY) / 3. SHIELD (HP +CCC, Mana Required: ZZZ)" << endl;
+              cin >> playerSkillChoice;
+              switch (playerSkillChoice){
+                case 1:
+                  if (playerMP >= XXX){
+                    playerDMG = AAA - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= XXX;
+                    cout << "Player's ETHEREAL CHAINS! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 2:
+                  if (playerMP >= YYY){
+                    playerDMG = BBB - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= YYY;
+                    cout << "Player's DISTORTION! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 3:
+                   if (playerMP >= ZZZ){
+                    playerHP += CCC;
+                    playerMP -= ZZZ;
+                    cout << "Player's SHIELD! HP + " << CCC << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                default:
+                  cout << "Invalid choice." << endl;
+                  break;
+              }
+            }
+            
+            else if (character == 3){
+              cout << "1. RAZOR SHURIKEN (Damage: AAA, Mana Required: XXX) / 2. SHADOW SLASH (Damage: BBB, Mana Required: YYY) / 3. DODGE (HP +CCC, Mana Required: ZZZ)" << endl;
+              cin >> playerSkillChoice;
+              switch (playerSkillChoice){
+                case 1:
+                  if (playerMP >= XXX){
+                    playerDMG = AAA - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= XXX;
+                    cout << "Player's RAZOR SHURIKEN! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 2:
+                  if (playerMP >= YYY){
+                    playerDMG = BBB - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= YYY;
+                    cout << "Player's SHADOW SLASH! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 3:
+                   if (playerMP >= ZZZ){
+                    playerHP += CCC;
+                    playerMP -= ZZZ;
+                    cout << "Player's DODGE! HP + " << CCC << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                default:
+                  cout << "Invalid choice." << endl;
+                  break;
+              }
+            }
+            
+            else if (character == 4){
+              cout << "1. ICATHIAN RAIN (Damage: AAA, Mana Required: XXX) / 2. VOID SEEKER (Damage: BBB, Mana Required: YYY) / 3. HEAL (HP +CCC, Mana Required: ZZZ)" << endl;
+              cin >> playerSkillChoice;
+              switch (playerSkillChoice){
+                case 1:
+                  if (playerMP >= XXX){
+                    playerDMG = AAA - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= XXX;
+                    cout << "Player's ICATHIAN RAIN! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 2:
+                  if (playerMP >= YYY){
+                    playerDMG = BBB - monsterDEF;
+                    monsterHP -= playerDMG;
+                    playerMP -= YYY;
+                    cout << "Player's VOID SEEKER! Damage: " << playerDMG << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                case 3:
+                   if (playerMP >= ZZZ){
+                    playerHP += CCC;
+                    playerMP -= ZZZ;
+                    cout << "Player's HEAL! HP + " << CCC << endl;
+                  }
+                  else {
+                    cout << "Not enough MP" << endl;
+                  }
+                  break;
+                  
+                default:
+                  cout << "Invalid choice." << endl;
+                  break;
+              }
+            }
+              
             break;
           
           case 3:
@@ -111,4 +304,42 @@ int main(){
             continue;
         }
         
+        monsterChoice = rand() % 2;
+        
+        if (monsterChoice == 0){                             //monster auto attack
+          monsterDMG = monsterATK - playerDEF;
+          AVD = rand() % 101 + playerAVD;
+          if (AVD >=100){
+            cout << "Monster attack miss!" << endl;
+          }
+          else {
+            playerHP -= monsterDMG;
+            cout << "You have been attacked by the monster! Damage: " << monsterDMG << endl;
+          }
+          
+        else if (monsterChoice == 1 && monsterMP >= XXX){                       //monster skills
+          monsterSkill = rand() % 2;
+          if (monsterSkill == 0){
+            monsterDMG = AAA - playerDEF;
+            playerHP -= monsterDMG;
+            monsterMP -= XXX;
+            cout << "Monster's SKILL 1! Damage: " << monsterDMG << endl;
+          }
+          else if (monsterSkill == 1){
+            monsterDMG = BBB - playerDEF;
+            playerHP -= monsterDMG;
+            monsterMP -= XXX;
+            cout << "Monster's SKILL 2! Damage: " << monsterDMG << endl;
+          }
+          
+      else if (monsterHP <= 0){
+        cout << "You have defeated the monster!" << endl << "VICTORY!" << "Game Terminated." << endl;
+        system("pause");
+      }
+        
+      else if (playerHP <= 0){
+        cout << "You have been defeated by the monster!" << endl << "DEFEAT!" << "Game Terminated." << endl;
+        system("pause");
+      }
+      
        
