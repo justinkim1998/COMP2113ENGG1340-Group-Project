b8c780ac3fcd9845c98a200674aa6fe069d95eb9
Seonhyeok Kim <90969155+justinkim1998@users.noreply.github.com>
2021-11-21 15:52:51 +0800

    Update preGame.h

--- preGame.h
+++ preGame.h
@@ -1,25 +1,17 @@
 #ifndef PREGAME_H
 #define PREGAME_H
 
-struct Status{
-  int playerHP;
-  int playerMP;
-  int playerATK;
-  int playerDEF;
-  int playerAVD;
-  int playerCRT;
-};
+struct Data{
+  int HP;       //HP for life
+  int MP;       //MP for skills 
+  int ATK;      //ATK for attack
+  int DEF;      //DEF for defense
+  int AVD;      //AVD for avoid rate
+  int CRT;      //CRT for critical damage 
+  int DMG;      //DMG for damage
+};  
 
-struct StatusM{
-  int monsterHP;
-  int monsterMP;
-  int monsterATK;
-  int monsterDEF;
-  int monsterAVD;
-  int monsterCRT;
-};
-
-void write_file(Status s);
-void read_file(Status &s);
+void write_file(Data s);
+void read_file(Data &s);
 
 #endif
