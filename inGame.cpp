40c8f4a6db635892e7686aeb5df1ec3d87012f4d
Seonhyeok Kim <90969155+justinkim1998@users.noreply.github.com>
2021-11-21 16:01:37 +0800

    Create inGame.cpp

---
+++ inGame.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+#include "inGame.h"
+
+//functions
+//bool meet_monster?
+//bool if_auto_attack?
+//bool if_skill?
+//bool if_escape?
+//void playerHP<=0
+//void monsterHP<=0
