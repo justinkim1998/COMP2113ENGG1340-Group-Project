78dc7b9aea5e435b7583078e92f998ed17e1beed
Seonhyeok Kim <90969155+justinkim1998@users.noreply.github.com>
2021-11-21 15:14:43 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -351,6 +351,44 @@ int main(){
   }
 } 
 
+
+
+
+//save&load file
+void write_file(Status &s){
+    ofstream foutStatus("player_status.txt");
+    if (foutStatus.fail()){
+        cout << "Error in player_status.txt writing!" << endl;
+        exit(1);
+    }
+    foutStatus <<  s.playerHP << " " << s.playerMP << " " << endl;
+    foutStatus.close();
+    
+    ofstream foutStatusM("monster_status.txt");
+    if (foutStatusM.fail()){
+        cout << "Error in monster_status.txt writing!" << endl;
+        exit(1);
+    }
+    foutStatusM << s.monsterHP << " " << s.monsterMP << " " << endl;
+    foutStatusM.close
+
+void read_file(Status &s){
+    ifstream finStatus("player_status.txt");
+    if (finStatus.fail()){
+        cout << "You have no data stored! Start from the beginning!" << endl;
+    }
+    else {
+        finStatus >> s.playerHP >> " " >> s.playerMP;
+        finStatus.close();
+        
+    ifstream finStatusM("monster_status.txt");
+    if (finStatusM.fail()){
+        cout << "You have no data stored! Start from the beginning!" << endl;
+    }
+    else{
+        finStatus >> s.monsterHP >> " " >> s.monsterMP;
+        finStatusM.close();
+
 // 이거 } 어디 분명 빼먹은거 같은데..
 // declare 안 한 variables 분명 있을듯..
 // 스탯 수치 정하기
