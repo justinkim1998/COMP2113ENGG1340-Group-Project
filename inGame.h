9c95ee99287dfe358f2a745f66d3bbf0c6bd34be
Seonhyeok Kim <90969155+justinkim1998@users.noreply.github.com>
2021-11-21 16:02:17 +0800

    Update inGame.h

--- inGame.h
+++ inGame.h
@@ -1,7 +1,12 @@
 #ifndef INGAME_H
 #define INGAME_H
 
-void function(xxx,yyy);
-void function2(xxx,yyy);
+//functions
+//bool meet_monster?
+//bool if_auto_attack?
+//bool if_skill?
+//bool if_escape?
+//void playerHP<=0
+//void monsterHP<=0
 
 #endif
